b79a13f080d88a29075bff15f00e072ac9c5cbce
Manish <mbb7224@gmail.com>
2024-01-03 23:12:40 +0530

    Added Register route & controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,7 +18,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
 
         //file has been uploaded successfully
-        console.log("File is uploaded successfully ", response.url);
+        // console.log("File is uploaded successfully ", response.url);
+        fs.unlinkSync(localFilePath)
         
         return response;
 
